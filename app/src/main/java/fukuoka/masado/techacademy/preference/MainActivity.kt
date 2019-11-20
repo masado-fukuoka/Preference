8df4f76b9d7e180a35c4cf5fdb5bf74d05778141
Masado Fukuoka <masado@eurovision.co.jp>
2019-11-20 12:53:10 +0900

    Initial commit

---
+++ app/src/main/java/fukuoka/masado/techacademy/preference/MainActivity.kt
@@ -0,0 +1,27 @@
+package fukuoka.masado.techacademy.preference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.Preference
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val preference = PreferenceManager.getDefaultSharedPreferences(this)
+
+        putButton.setOnClickListener {
+            val editor = preference.edit()
+            editor.putString("TEXT", editText.text.toString())
+            editor.commit()
+        }
+
+        getButton.setOnClickListener {
+            editText.setText(preference.getString("TEXT","まだ保存されていません"))
+        }
+    }
+}
